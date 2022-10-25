3ff6611050218533eb27b2d4096590b51df6ec0a
ceyydoee <ceyquanreavis@yahoo.com>
2022-10-25 10:55:24 -0700

    birthday

---
+++ main.py/animal.py
@@ -0,0 +1,12 @@
+class animal():
+    def __init__(self):
+        self.type=""
+        self.size=""
+        self.age=0
+        self.name=""
+
+    def speak(self):
+        pass
+    def describe(self):
+        pass
+        
